7d98d60afa753b120b56b628fd1537060080e301
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-17 20:13:51 +0530

    serialization coding example

--- src/main/java/org/example/SerializeDemo.java
+++ src/main/java/org/example/SerializeDemo.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.io.*;
+
 public class SerializeDemo {
     public static void main(String[] args) {
         Employee e = new Employee();
@@ -7,5 +9,21 @@ public class SerializeDemo {
         e.address = "Galle";
         e.NIC = 112233;
         e.number = 119;
+
+        try {
+            //create object from FileOutputStream and give the serializable type file name
+            FileOutputStream fileOut = new FileOutputStream("employee.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            //pass the object that we want to write
+            out.writeObject(e);
+            //close the FileOutputStream and ObjectOutputStream
+            out.close();
+            fileOut.close();
+
+            System.out.println("serialized date is saved in employee.ser");
+
+        }catch (IOException i){
+            i.printStackTrace();
+        }
     }
 }
