82a43bd0fa67a0ce7aab5a6af4cdab8619554229
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-17 19:25:01 +0530

    assign values to attributes of emplyee class

--- src/main/java/org/example/SerializeDemo.java
+++ src/main/java/org/example/SerializeDemo.java
@@ -3,5 +3,9 @@ package org.example;
 public class SerializeDemo {
     public static void main(String[] args) {
         Employee e = new Employee();
+        e.name = "Uchithma Senevirathne";
+        e.address = "Galle";
+        e.NIC = 112233;
+        e.number = 119;
     }
 }
