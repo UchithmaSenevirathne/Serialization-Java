e03aa3c6296c21ed289cf8bdce6aa8c12e1e4a37
UchithmaSenevirathne <uchithma@gmail.com>
2024-07-17 19:15:30 +0530

    create attributes in employee

--- src/main/java/org/example/Employee.java
+++ src/main/java/org/example/Employee.java
@@ -1,5 +1,9 @@
 package org.example;
 
 public class Employee implements java.io.Serializable{
+    public String name;
+    public String address;
+    public transient int NIC;
+    public int number;
 
 }
